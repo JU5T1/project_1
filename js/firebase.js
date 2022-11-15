6bb65c5d0ef9c851d12cd72eade49d10e1e63789
JU5T1 <65051873+JU5T1@users.noreply.github.com>
2022-11-15 14:02:39 +0000

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBzKR83w2egeI71VyDmLJtOPfWwUOrzLjU",
+    authDomain: "blogging-website-ccdf6.firebaseapp.com",
+    projectId: "blogging-website-ccdf6",
+    storageBucket: "blogging-website-ccdf6.appspot.com",
+    messagingSenderId: "943920642258",
+    appId: "1:943920642258:web:6f2589bbd5bfcd8fe5950b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore(); 
